fcae041b3af844df0f67eb3d244bac2bff8a5c21
null8572 <81449994+null8572@users.noreply.github.com>
2021-03-30 09:14:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,17 @@
-from discord.ext import commands
 import os
 import traceback
 import discord
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_message(message):
+    if message.content == '/start':
+        message = random.randint(1,10)
+        await message.channel.send(num)
+    await message.channel.send('debug::') 
 
-@bot.command()
-async def start(ctx):
-    await ctx.send('pong')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    message = random.randint(1,10)
-    await message.channel.send(num)
     
-    
-bot.run(token)
-
+client.run(token)    
