c5f1ec31278e4d99df5a7ccb3919e996a5ab8275
null8572 <81449994+null8572@users.noreply.github.com>
2021-03-30 09:00:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -12,7 +12,7 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
+@client.event
 async def on_message(message):
     if message.content == "!r start":
         message = random.randint(1,10)
